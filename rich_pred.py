ff8bc340bf47bd28ff97467c536858795c7f9398
lk1818 <lk1818@nyu.edu>
2017-02-02 16:15:35 -0500

    Correction of rich_pred.py by Li kE v2

--- rich_pred.py
+++ rich_pred.py
@@ -49,7 +49,7 @@ sar_eqs = [power_eq, power_quad_eq, logarithmic_eq, michaelis_menten_eq, lomolin
 sar_parameters = [[22.7, 0.3], [1.2, 0.163, 0.009],
                   [14.36, 21.16], [85.91, 42.57],
                   [1082.45, 1.59, 390000000]]
-                  
+
 if __name__ == "__main__":
     for area in fileinput.input():
         print(area.strip() + ',' + str(get_sar_estimate(float(area), sar_eqs, sar_parameters)))
